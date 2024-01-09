a6ab3570b2294282fd22840d8426441a255fbd1f
MilenePergon <52928494+milenepergon@users.noreply.github.com>
2024-01-09 17:29:31 -0300

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,14 @@
+#Passo a passo do projeto
+#1-Login https://dlp.hashtagtreinamentos.com/python/intensivao/login
+#2- Importar base de dados 
+#3- Cadastrar produto
+#4- Cadastrar Repetir até acabar base
+# instalar libbs pyautogui
+import pyautogui
+
+ 
+#clicar >pyautogui.click
+#escrever >pyautogui.write
+#apertar uma tecla >pyautogui.press
+#clicar >pyautogui.click
+pyautogui - press ("win")
